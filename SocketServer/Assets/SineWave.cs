e511bc3640d27f6d0dca0f47bb33e1ab6ceff8bf
Tyler Cauthen <tcauthe1@gmail.com>
2017-09-13 16:54:55 -0400

    Demo2

---
+++ SocketServer/Assets/SineWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWave : MonoBehaviour {
+	public int numberOfDots = 28;
+	public GameObject[] waveDots;
+	public float factor;
+	public float amplitude;
+
+	void Update() {
+		for (int i = 0; i < numberOfDots; i++) {
+			Vector3 position = waveDots [i].transform.localPosition;
+			position.y = Mathf.Sin (Time.time + i * factor) * amplitude;
+			waveDots [i].transform.localPosition = position;
+		}
+	}
+}
---
+++ SocketServer/Assets/rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.RotateBy( gameObject, iTween.Hash(
+			"amount", new Vector3(0f,0f,0.05f),
+			"time"    , 2,
+			"looptype", "pingPong",
+			"easetype", iTween.EaseType.easeInOutSine
+		));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
